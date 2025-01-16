3c9f036071561967e80ccc4f77365fb3c6c3a6b7
Naguib <moma.naguib@gmail.com>
2025-01-16 23:02:19 +0100

    add header component with authentication links and styling

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isLoggedIn$;
 
+  constructor(private authService: AuthService) {
+    this.isLoggedIn$ = this.authService.isAuthenticated$;
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
